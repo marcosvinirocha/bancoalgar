ae8c1a37d815f1b8bcb11173f2914f3a52eb411f
marcos <marcosvinicius.udia1256@gmail.com>
2021-12-05 11:12:08 -0300

    update

--- src/main/java/com/unibank/algar/bancoalgar/exception/ErrorDetails.java
+++ src/main/java/com/unibank/algar/bancoalgar/exception/ErrorDetails.java
@@ -1,4 +1,4 @@
-package com.unibank.exception;
+package com.unibank.algar.bancoalgar.exception;
 
 import java.util.Date;
 
--- src/main/java/com/unibank/algar/bancoalgar/exception/ResourceNotFoundException.java
+++ src/main/java/com/unibank/algar/bancoalgar/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.unibank.exception;
+package com.unibank.algar.bancoalgar.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
